42d4242f4b0e1253bbd334fa8380ba2529e0cf18
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-08-01 20:04:04 -0400

    README cli math-tower

--- README.md
+++ README.md
@@ -1,32 +1,45 @@
-# Codex xegos 
+# Codex Xegos 
 
 ## Installation
 
 1. git clone
-2. `npm install`
+2. cd into xegos and `npm install`
 3. `npm install -g ./`. This will put `xego` on your path
 4. `cd` to whatever project you want to make xegos in
 5. Put your OAI key in `.env` as `OPENAI_API_KEY`
 
 ## Create a New xego
 
-To create a new xego run `xego -command | --c "your command here" --name "your-xego-name"`
+To create a new xego run `xego --command | -c "your command here" --name "your-xego-name"`
 
 This will create a file in your directory called `{command}.js`
 
 For more complex xegos, you can pass in other xegos as arguments from a file. Post them in order. 
-For example `npm run n-xego -command | --c "your command here" --xegos xego.txt`
+For example `npm run n-xego -command | --c "your command here" --path xego.txt`
 
-## Edit your xego
+## Xego Tower
 
-## Combine xegos
+A Xego tower is a txt file that uses oold xegos as a base for new xegos.
 
-To compose xegos, you can import them as you would any other JS file.
+### Format
 
-## Send your xego to GitHub Gist or Repl.it
+```
+Language: {js,css,py,html,etc.}
+xego-1
+xego-2
+...
+```
+
+See `xego-recipe.txt` for an example.
+
+## Help?
+
+`xego --help`
+
+## Send your xego to GitHub Gist
 
 todo...
 
-## Xego Repository
+## Xego Repository -- Find Other People's Xegos!
 
 todo...
--- cli.ts
+++ cli.ts
@@ -6,6 +6,14 @@ import { writeXegoToFile } from './xego-fs';
 const dotenv = require('dotenv');
 dotenv.config();
 
+interface Options {
+    name: string;
+    command: string;
+    path: string;
+    language: string;
+    maxTokens: number;
+    temperature: number;
+}
 
 program
     .description('A command line tool for creating Xegos')
@@ -13,9 +21,9 @@ program
     .option('-n, --name <name>','Name of the Xego', null)
     .requiredOption('-c, --command <command>', 'Command for codex to execute')
     .option('-p, --path <path>', 'Path to Xego Tower', null)
-    .option('-temp --temperature', 'Temperature of the Xego', 0.1)
-    .option('-mt --maxTokens', 'Max number of tokens', 150)
-    .option('-l, --language <language>', 'Language of the Xego. See languages at https://github.com/bramses/xegos/blob/master/file-endings.ts', 'js')
+    .option('-temp, --temperature <temperature>', 'Temperature of the Xego', 0.1)
+    .option('-mt, --maxTokens <maxTokens>', 'Max number of tokens', 150)
+    .option('-l, --language <language>', 'Language of the Xego. See supported languages at https://github.com/bramses/xegos/blob/master/file-endings.ts', 'js')
 
 
 program.parse(process.argv);
@@ -24,9 +32,13 @@ if (!process.env.OPENAI_API_KEY) {
     throw new Error('OPENAI_API_KEY environment variable is not set');
 }
 
-const main = async (options) => {
-    const xego = await createXegoBlock(options.name, options.command, options.path, options.language, options.maxTokens, options.temperature);
-    await writeXegoToFile(xego);
+const main = async (options: Options) => {
+    try {
+        const xego = await createXegoBlock(options.name, options.command, options.path, options.language, options.maxTokens, options.temperature);
+        await writeXegoToFile(xego);
+    } catch (err) {
+        throw err;
+    }
 }
 
 
